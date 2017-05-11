2e4861bbf3b38b7fd77e88340f95dc2b8033ed6f
Simonchelbc <simon.lebail-collet@epfl.ch>
2017-05-11 13:13:57 +0200

    change logger outputstreamwriter encoding to utf-8

--- app/services/Logger.scala
+++ app/services/Logger.scala
@@ -2,6 +2,7 @@ package services
 
 import javax.inject._
 import java.util.concurrent.ExecutorService
+import java.nio.charset.Charset
 
 
 //a Runnable class writing an inputText to a certain file
@@ -11,8 +12,12 @@ class WriterJob(val inputText: String, val fileName: String) extends java.util.c
         }
 
         def printToFile(f: java.io.File)(op: java.io.PrintWriter => Unit) {
-                val fw = new java.io.FileWriter(f, true) // true allows appending data to current file content
-                val bw = new java.io.BufferedWriter(fw)
+                val fw = new java.io.FileOutputStream(f, true) // true allows appending data to current file content
+		val osw = new java.io.OutputStreamWriter(
+				fw, 
+				Charset.forName("UTF-8").newEncoder()
+		)
+                val bw = new java.io.BufferedWriter(osw)
                 val p = new java.io.PrintWriter(bw)
                 try { op(p) } finally { p.close() }
         }
